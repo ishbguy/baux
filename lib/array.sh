8f1a0fa88be0460b5524fc1ec2f4612810de342c
Herbert Shen <ishbguy@hotmail.com>
2018-03-24 12:13:11 +0800

    feat: add more array funcs

--- lib/array.sh
+++ lib/array.sh
@@ -9,7 +9,7 @@
 # source guard
 [[ $BAUX_ARRAY_SOURCED -eq 1 ]] && return
 declare -gr BAUX_ARRAY_SOURCED=1
-declare -gr BAUX_ARRAY_ABS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
+declare -gr BAUX_ARRAY_ABS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null; pwd)
 
 # source dependences
 if [[ $BAUX_SOUECED -ne 1 ]]; then
@@ -69,7 +69,7 @@ slice() {
 
     for idx in "$@"; do
         [[ $idx -ge 0 ]] || die "$idx is little than 0."
-        [[ -n ${__array[$idx]} ]] || continue
+        [[ -n $idx && -n ${__array[$idx]} ]] || continue
         slices+=("${__array[$idx]}")
     done
 
@@ -98,7 +98,7 @@ exists() {
     is_array "$1" || is_map "$1" || die "$1 is not an array name."
     
     local -n __array="$1"
-    [[ -n ${__array[$2]} ]]
+    [[ -n $2 && -n ${__array[$2]} ]]
 }
 
 _join() {
@@ -132,6 +132,142 @@ _split() {
     # end is sep, append "" to array
     [[ $1 =~ $sep$ ]] && __array+=("")
     return 0
+}
+
+_uniq() {
+    local -A uniq
+    for it in "$@"; do
+        [[ $it == "" ]] && continue
+        uniq[$it]=1
+    done
+    echo "${!uniq[@]}"
+}
+
+__swap() {
+    local -n __array="$1"
+    local tmp=${__array[$2]}
+    __array[$2]=${__array[$3]}
+    __array[$3]=$tmp
+}
+
+__cmp() { [[ $1 -gt $2 ]]; }
+
+__issorted() {
+    local -a array=("$@")
+    local len=${#array[@]}
+    for ((i = 0; i < $[$len-1]; i++)); do
+        __cmp ${array[$i]} ${array[$i+1]} && return 1
+    done
+    return 0
+}
+
+_sort() {
+    local opt="-n"
+    eval "[[ $1 =~ ^- ]]" &>/dev/null && opt="$1" && shift
+    echo "$@" | sed -r 's/\s+/\n/g' | sort "$opt"
+}
+
+# below search algo just for practise :)
+
+select_sort() {
+    local cmp=__cmp
+    is_func "$1" && cmp=$1 && shift
+    local -a array=("$@")
+
+    for ((i = 0; i < ${#array[@]}; i++)); do
+        for ((j = i+1; j < ${#array[@]}; j++)); do
+            $cmp ${array[$i]} ${array[$j]} && __swap array $i $j
+        done
+    done
+    echo "${array[@]}"
+}
+
+insert_sort() {
+    local cmp=__cmp
+    is_func "$1" && cmp=$1 && shift
+    local -a array=("$@")
+    
+    for ((i = 1; i < ${#array[@]}; i++)); do
+        for ((j = i; j > 0; j--)); do
+            $cmp ${array[$j-1]} ${array[$j]} && __swap array $j $[$j-1]
+        done
+    done
+    echo "${array[@]}"
+}
+
+shell_sort() {
+    local cmp=__cmp
+    is_func "$1" && cmp=$1 && shift
+    local -a array=("$@")
+    local l=${#array[@]}
+    local h=1
+    
+    while ((h < l/3)); do h=$((3*h+1)); done
+    
+    while ((h>=1)); do
+        for ((i = h; i < ${#array[@]}; i++)); do
+            for ((j = i; j >= h; j = j-h)); do
+                $cmp ${array[$j-$h]} ${array[$j]} \
+                    && __swap array $j $[$j-$h]
+            done
+        done
+        h=$((h/3))
+    done
+    echo "${array[@]}"
+}
+
+search() {
+    ensure "$# -ge 1" "Need at least an array name."
+    is_array "$1" || is_map "$1" || die "$1 is not an array name."
+
+    local -n __array="$1"; shift
+    local -a indexs=()
+    local -A keys
+    for idx in "${!__array[@]}"; do
+        keys[${__array[$idx]}]="$idx"
+    done
+    for need in "$@"; do
+        [[ -n $need && -n ${keys[$need]} ]] \
+            && indexs+=("${keys[$need]}")
+    done
+    echo "${indexs[@]}"
+}
+
+lsearch() {
+    ensure "$# -eq 2" "Need an array name and a item"
+    is_array "$1" || is_map "$1" || die "$1 is not an array name."
+
+    local -n __array="$1"
+    local need="$2"
+    for idx in "${!__array[@]}"; do
+        [[ ${__array[$idx]} == $need ]] && echo $idx && return 0
+    done
+    return 1
+}
+
+bsearch() {
+    ensure "$# -eq 2" "Need an array name and a item"
+    local cmp=__cmp
+    is_func "$1" && cmp=$1 && shift
+    is_array "$1" || is_map "$1" || die "$1 is not an array name."
+
+    local -n __array="$1"
+    local need="$2"
+    local low mid hi
+    
+    low=0
+    hi=$((${#__array[@]}-1))
+    while ((low <= hi)); do
+        mid=$(((low+hi)/2))
+        [[ ${__array[$mid]} == $need ]] && echo $mid && return 0
+        if __cmp ${__array[$mid]} $need ; then
+            hi=$((mid-1))
+
+        else
+            low=$((mid+1))
+        fi
+    done
+    return 1
 }
 
 # vim:ft=sh:ts=4:sw=4
--- test/07-test-array.bats
+++ test/07-test-array.bats
@@ -222,3 +222,49 @@ source $SRC_DIR/lib/array.sh
     _split "one  three " array ":"
     [[ ${array[0]} == "one  three " ]]
 }
+
+@test "test sort" {
+    array=({10..0})
+
+    __issorted $(_sort $(_sort -R ${array[@]}))
+    __issorted $(select_sort $(_sort -R ${array[@]}))
+    __issorted $(insert_sort $(_sort -R ${array[@]}))
+    __issorted $(shell_sort $(_sort -R ${array[@]}))
+}
+
+@test "test search" {
+    array=({1..100})
+
+    run search array 49
+    [[ $output == "48" ]]
+
+    run search array 100
+    [[ $output == "99" ]]
+
+    run search array 49 100
+    [[ $output == "48 99" ]]
+}
+
+@test "test lsearch" {
+    array=({0..1000})
+
+    NEED=$((RANDOM % 1000))
+    run lsearch array  $NEED
+    [[ $output == $NEED ]]
+
+    NEED=$((RANDOM % 1000))
+    run lsearch array $NEED
+    [[ $output == $NEED ]]
+}
+
+@test "test bsearch" {
+    array=({0..10})
+
+    NEED=$((RANDOM % 10))
+    run bsearch array $NEED
+    [[ $output == $NEED ]]
+
+    NEED=$((RANDOM % 10))
+    run bsearch array $NEED
+    [[ $output == $NEED ]]
+}
